97e0129ca1aee40ec3f3a1f52f55b2caee3b9d5f
cimbo92 <alessandro.cimbelli@mail.polimi.it>
2014-12-18 08:53:45 +0100

    Registration and authentication

---
+++ MeteoCalendarium/src/main/java/RegistrationAuthentication/gui/security/UserBean.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RegistrationAuthentication.gui.security;
+
+import RegistrationAuthentication.business.security.boundary.UserManager;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author home
+ */
+@Named
+@RequestScoped
+public class UserBean {
+
+    
+    @EJB
+    UserManager um;
+    
+    public UserBean() {
+    }
+    
+    public String getName() {
+        return um.getLoggedUser().getName();
+        
+    }
+}
