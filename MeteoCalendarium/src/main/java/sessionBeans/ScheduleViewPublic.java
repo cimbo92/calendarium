6824e6717c3e4e360134e9a0a91aeaebcfe1ad81
Alessandro <Alessandro@192.168.1.78>
2015-01-09 18:19:39 +0100

    -commit cianfe

--- MeteoCalendarium/src/main/java/sessionBeans/ScheduleViewPublic.java
+++ MeteoCalendarium/src/main/java/sessionBeans/ScheduleViewPublic.java
@@ -8,6 +8,7 @@ package sessionBeans;
 import entities.Event;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
@@ -18,40 +19,34 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import managerBeans.EventManagerInterface;
 import managerBeans.UserManagerInterface;
+import org.primefaces.context.RequestContext;
 import org.primefaces.model.DefaultScheduleEvent;
 import org.primefaces.model.DefaultScheduleModel;
 import org.primefaces.model.ScheduleModel;
-
-
 
 /**
  *
  * @author Alessandro
  */
 
+@ViewScoped
 @Named
 public class ScheduleViewPublic implements Serializable {
- 
-  private ScheduleModel eventModel;
-   
+
     @EJB
   private EventManagerInterface em;
     
      @EJB
   private UserManagerInterface um;
-   
-private String username;
 
-private List<String> users =new ArrayList<>();
+  private List<String> users =new ArrayList<>();
+  private Event event;
+ 
+    private ScheduleModel eventModel;
 
-    public List<String> getUsers() {
-        return users;
-    }
+    private String username;
 
-    public void setUsers(List<String> users) {
-        this.users = users;
-    }
-
+    
     public String getUsername() {
         return username;
     }
@@ -59,15 +54,31 @@ private List<String> users =new ArrayList<>();
     public void setUsername(String username) {
         this.username = username;
     }
-     
+
+    public List<String> getUsers() {
+        return users;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public void setUsers(List<String> users) {
+        this.users = users;
+    }
+    
     @PostConstruct
     public void init() {
         eventModel = new DefaultScheduleModel();   
         users =  um.getListUsers();  
     }
-     
+    
     public void loadCalendar(){
-
+               
         List<Event> tempCalendar = em.loadPublicCalendar(username);
         for (Event tempCalendar1 : tempCalendar) {
            DefaultScheduleEvent temp = new DefaultScheduleEvent(tempCalendar1.getTitle(), tempCalendar1.getStartDate(), tempCalendar1.getEndDate());
@@ -86,8 +97,7 @@ private List<String> users =new ArrayList<>();
     public ScheduleModel getEventModel() {
         return eventModel;
     }
-     
-
 }
+
 
 
