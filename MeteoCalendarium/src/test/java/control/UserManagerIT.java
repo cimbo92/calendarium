dee227ef167e3200317186a40311895815560f00
cianfe <alessandro.cianferotti@mail.polimi.it>
2015-01-23 12:04:55 +0100

    arqu user

--- MeteoCalendarium/src/test/java/control/UserManagerIT.java
+++ MeteoCalendarium/src/test/java/control/UserManagerIT.java
@@ -37,164 +37,101 @@ import static org.mockito.Mockito.verify;
  */
 @RunWith(Arquillian.class)
 public class UserManagerIT {
-    
+
     public UserManagerIT() {
     }
-    
-@EJB
+
+    @EJB
     private UserManagerInterface um;
     @PersistenceContext
     private EntityManager em;
-    
+
     @Inject
     Principal principal;
-    
-    
-    private UserManager cut;
-    
+
+    User user1;
+    User user2;
+
     @Deployment
-    public static WebArchive createArchiveAndDeploy(){
+    public static WebArchive createArchiveAndDeploy() {
         return ShrinkWrap.create(WebArchive.class)
                 .addClass(UserManagerInterface.class)
-                .addClass(EntityManager.class)     
+                .addClass(EntityManager.class)
                 .addPackage(User.class.getPackage())
                 .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
                 .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
-        
+
     }
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-    
     @Before
     public void prepareTests() throws Exception {
         clearData();
         insertData();
     }
-    
-    @Before
-    public void setUp() {
-        cut = new UserManager();
-        cut.setEm(mock(EntityManager.class));
-    }
-
-    @After
-    public void tearDown() {
-    }
-
-    @Test
-    public void newUsersShouldBelongToUserGroupAndSavedOnce() throws Exception {
-        User newUser = new User();
-        newUser.setEmail("prova");
-        cut.save(newUser);
-        assertThat(newUser.getGroupName(), is(Group.USER));
-
-        verify(cut.getEm(), times(1)).persist(newUser);
-    }
 
     /**
      * Test of getListUsers method, of class UserManager.
-     *//*
+     */
     @Test
     public void testGetListUsers() throws Exception {
-        System.out.println("getListUsers");
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        UserManagerInterface instance = (UserManagerInterface)container.getContext().lookup("java:global/classes/UserManager");
-        List<String> expResult = null;
-        List<String> result = instance.getListUsers();
-        assertEquals(expResult, result);
-        container.close();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }*/
-
-   
+        List<String> users = um.getListUsersPublic();
+        assertEquals(um.findByMail(users.get(0)), user1);
+        assertEquals(um.findByMail(users.get(1)), user2);
+    }
 
     /**
      * Test of setCalendar method, of class UserManager.
-     *//*
+     */
     @Test
     public void testSetCalendar() throws Exception {
-        System.out.println("setCalendar");
-        boolean status = false;
-        User user = null;
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        UserManagerInterface instance = (UserManagerInterface)container.getContext().lookup("java:global/classes/UserManager");
-        instance.setCalendar(status, user);
-        container.close();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }*/
-    
+        User user = um.findByMail("user1@mail.it");
+        assertEquals(false, user.isPublicCalendar());
+        um.setCalendar(true, user);
+        assertEquals(true, user.isPublicCalendar());
+    }
 
     /**
      * Test of getListUsersPublic method, of class UserManager.
-     *//*
+     */
     @Test
     public void testGetListUsersPublic() throws Exception {
-        System.out.println("getListUsersPublic");
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        UserManagerInterface instance = (UserManagerInterface)container.getContext().lookup("java:global/classes/UserManager");
-        List<String> expResult = null;
-        List<String> result = instance.getListUsersPublic();
-        assertEquals(expResult, result);
-        container.close();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }*/
-
-
+        List<String> users = um.getListUsersPublic();
+        assertEquals("user2@mail.it", um.findByMail(users.get(0)));
+    }
 
     /**
      * Test of save method, of class UserManager.
      */
     @Test
     public void testSave() throws Exception {
-     
+
         User user1 = new User();
         user1.setEmail("user1@mail.it");
         user1.setPassword("user1");
         user1.setGroupName(Group.USER);
         user1.setPublicCalendar(true);
         um.save(user1);
-        assertEquals(user1,um.findByMail(user1.getEmail()));
+        assertEquals(user1, um.findByMail(user1.getEmail()));
     }
 
-
-
-    /**
-     * Test of getLoggedUser method, of class UserManager.
-     *//*
-    @Test
-    public void testGetLoggedUser() throws Exception {
-        System.out.println("getLoggedUser");
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        UserManagerInterface instance = (UserManagerInterface)container.getContext().lookup("java:global/classes/UserManager");
-        User expResult = null;
-        User result = instance.getLoggedUser();
-        assertEquals(expResult, result);
-        container.close();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    */
-    
-     private void clearData() throws Exception {
+    private void clearData() throws Exception {
         //em.joinTransaction();
         System.out.println("Dumping old records...");
         em.createQuery("delete from User").executeUpdate();
     }
-    
-      private void insertData() throws Exception {
-        User user1 = new User();
+
+    private void insertData() throws Exception {
+        user1 = new User();
         user1.setEmail("user1@mail.it");
         user1.setPassword("user1");
         user1.setGroupName(Group.USER);
+        user1.setPublicCalendar(false);
         em.persist(user1);
-      }    
+        user2 = new User();
+        user2.setEmail("user2@mail.it");
+        user2.setPassword("user2");
+        user2.setGroupName(Group.USER);
+        user2.setPublicCalendar(true);
+        em.persist(user2);
+    }
 }
